2a910a1c7826e34dd362fc4d4e492dd111d3bafe
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2014-05-02 20:31:03 +0900

    Use upper case for SQL.

--- make_db_init.c
+++ make_db_init.c
@@ -6,43 +6,43 @@
 #include "device_address.c"
  
 #define SQL_CREATE_SUPPORTED_DEVICES \
-  "create table supported_devices(" \
-    "device_id integer primary key asc, " \
-    "device text not null, " \
-    "build_id text not null, " \
-    "check_property_name text, " \
-    "check_property_value text, " \
-    "unique (device, build_id, check_property_name, check_property_value));" \
+  "CREATE TABLE supported_devices(" \
+    "device_id INTEGER PRIMARY KEY ASC, " \
+    "device TEXT NOT NULL, " \
+    "build_id TEXT NOT NULL, " \
+    "check_property_name TEXT, " \
+    "check_property_value TEXT, " \
+    "UNIQUE (device, build_id, check_property_name, check_property_value));" \
 
 #define SQL_CREATE_DEVICES_ADDRESS \
-  "create table device_address(" \
-    "device_id integer not null, " \
-    "name text not null, " \
-    "value integer not null, " \
-    "primary key(device_id, name));" \
+  "CREATE TABLE device_address(" \
+    "device_id INTEGER NOT NULL, " \
+    "name TEXT NOT NULL, " \
+    "value INTEGER NOT NULL, " \
+    "PRIMARY KEY(device_id, name));" \
 
 #define SQL_INSERT_SUPPORTED_DEVICES \
-  "insert into supported_devices(" \
+  "INSERT INTO supported_devices(" \
     "device_id, " \
     "device, " \
     "build_id, " \
     "check_property_name, " \
     "check_property_value) " \
-  "values(" \
+  "VALUES(" \
     "%d, " \
     "'%s', " \
     "'%s', " \
-    "null, " \
-    "null);"
+    "NULL, " \
+    "NULL);"
 
 #define SQL_INSERT_SUPPORTED_DEVICES_WITH_CHECK_PROPERTY \
-  "insert into supported_devices(" \
+  "INSERT INTO supported_devices(" \
     "device_id, " \
     "device, " \
     "build_id, " \
     "check_property_name, " \
     "check_property_value) " \
-  "values(" \
+  "VALUES(" \
     "%d, " \
     "'%s', " \
     "'%s', " \
@@ -50,11 +50,11 @@
     "'%s');"
 
 #define SQL_INSERT_DEVICES_ADDRESS \
-  "insert into device_address(" \
+  "INSERT INTO device_address(" \
     "device_id, " \
     "name, " \
     "value) " \
-  "values(" \
+  "VALUES(" \
     "%d, " \
     "'%s', " \
     "%u);"
