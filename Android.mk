c4781cc28bcdee00d4ed52db9a9317ad36b81d64
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2014-05-03 08:35:58 +0900

    Rename MY_LOCAL_PATH to DEVICE_DATABASE_LOCAL_PATH
    The path name should be unique.

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
-MY_LOCAL_PATH := $(call my-dir)
+DEVICE_DATABASE_LOCAL_PATH := $(call my-dir)
 
-LOCAL_PATH := $(MY_LOCAL_PATH)
+LOCAL_PATH := $(DEVICE_DATABASE_LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
@@ -15,4 +15,4 @@ LOCAL_STATIC_LIBRARIES += libsqlite
 
 include $(BUILD_STATIC_LIBRARY)
 
-include $(MY_LOCAL_PATH)/libsqlite/Android.mk
+include $(DEVICE_DATABASE_LOCAL_PATH)/libsqlite/Android.mk
